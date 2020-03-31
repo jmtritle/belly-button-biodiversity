ff14548ed381815054015d4c1e9396bb4299fdba
jmtritle <57117317+jmtritle@users.noreply.github.com>
2020-03-30 20:13:19 -0400

    small adjustments

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,6 @@
-d3.json("../data/samples.json").then(function(data) {
+d3.json("./data/samples.json").then(function(data) {
     console.log(data);
 });
+
+const dataPromise = d3.json("./data/samples.json");
+console.log("Data Promise: ", dataPromise);
