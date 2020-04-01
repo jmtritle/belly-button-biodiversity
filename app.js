6b5e2e04a67bda37137a037d535588af73d71954
jmtritle <57117317+jmtritle@users.noreply.github.com>
2020-03-31 22:47:13 -0400

    updates

--- app.js
+++ app.js
@@ -3,33 +3,111 @@
 d3.json("./data/samples.json").then(function(data) {
     console.log(data);
 });
+
 // setting up the datapromise and making sure it's pulling correctly
 const dataPromise = d3.json("./data/samples.json");
 console.log("Data Promise: ", dataPromise);
 
-// setting up an unpack function
-function unpack(rows, index) {
-    return rows.map(function(row) {
-      return row[index];
-    });
-  };
+var dropDown = d3.select("#selDataset")
+  .selectAll("option")
+  .data(data.names)
+  .enter()
+  .append("option")
+  .text(d => d);
+
+function changeOption(value) {
+  console.log(value);
+  updatePlotly(value);
+}
+
+function updatePlotly(value) {
+  d3.json("./samples/json").then(function(data) {
+    var samples = data.samples;
+    var otuData = [];
+
+    for (var i = 0; i < samples.length; i++) {
+      if samples[i].id === value) {
+        for (var o = 0; o < samples[i].otu_ids.length; o++) {
+          otuData.push({
+            otu_id: samples[i].otu_ids[o],
+            sample_value: samples[i].sample_values[o],
+            otu_label: samples[i].otu_labels[o]
+          })
+        }
+      }
+    }
+  })
 
 
-// setting the y axis and labels variables
-var y = Object.values(data.samples);
-var labels = Object.keys(data.otu_ids);
+// sorting the data appropriately
+var dataSort = otuData;
+dataSort.sort((firstValue, secondValue) => secondValue.sample_values - firstValue.sample_values);
 
-// setting up the dropdown menu
-d3.selectAll("#selDataset").on("change", updatePlotly);
-    // Use D3 to select the dropdown menu
-    var dropdownMenu = d3.select("#selDataset");
-    // Assign the value of the dropdown menu option to a variable
-    var dataset = dropdownMenu.property("value");
+// taking the sorted info and slicing it up, then reversing the order
+var dataslice = dataSort.slice(0, 10).reverse();
 
-    var id = dataset.id;
-    var samples = dataset.samples;
-    var labels = dataset.otu_labels;
-  
-function updatePlotly()
+// slicing things up from the map
+// x is the sample_values from the dataset
+// y is the otu_ids from the dataset
+// hoverLabel is the otu_labels from the dataset
+var x = dataslice.map(d => d.sample_values);
+var y = dataslice.map(d => `OTU ${d.otu_ids}`);
+var hoverLabel = dataslice.map(d => d.otu_labels);
 
+// setting up the trace information so it pulls the right info
+// and makes it a horizontal bar chart
+var trace1 = {
+  x: x,
+  y: y,
+  hovertext: hoverLabel,
+  type: "bar",
+  orientation: "h"
+};
+
+// setting the title for the layout
+var layout = {
+  title: "Top 10 OTUs",
+};
+
+// setting the data variable for plotting purposes
+var data = [trace1];
+
+// rigging up ye olde plotly plot
+Plotly.newPlot("bar-chart", data, layout);
+
+// bubble chart is a go!
+// all the trace variables, oh boy
+
+var x = otuData.map(d => d.otu_ids);
+var y = otuData.map(d => d.sample_values);
+var size = otuData.map(d => d.sample_values);
+var color = otuData.map(d => d.otu_ids);
+var text = otuData.map(d => d.otu_labels);
+
+// setting trace data
+
+var trace1 = {
+  x: x,
+  y: y,
+  mode: "markers",
+  marker: { 
+    color: color,
+    size: size
+  },
+  hovertext: text
+};
+
+var layout = {
+  xaxis: {
+    title: "OTU ID"
+  }
+};
+
+var data = [trace1];
+
+Plotly.newPlot("bubble-chart", data, layout);
+
+});
+
+};
   init();
